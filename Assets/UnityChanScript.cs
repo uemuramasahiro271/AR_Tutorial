aee64586a949a7bee1d8178e3814ffa0121fcb69
uemuramasahiro271 <uemura.free2020@gmail.com>
2021-02-14 20:00:23 +0900

    初回コミット

---
+++ Assets/UnityChanScript.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnityChanScript : MonoBehaviour
+{
+    private Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            anim.SetBool("Walk_F", true);
+        }
+        else
+        {
+            anim.SetBool("Walk_F", false);
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            anim.SetBool("Walk_B", true);
+        }
+        else
+        {
+            anim.SetBool("Walk_B", false);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            anim.SetBool("Walk_R", true);
+        }
+        else
+        {
+            anim.SetBool("Walk_R", false);
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            anim.SetBool("Walk_L", true);
+        }
+        else
+        {
+            anim.SetBool("Walk_L", false);
+        }
+    }
+}
